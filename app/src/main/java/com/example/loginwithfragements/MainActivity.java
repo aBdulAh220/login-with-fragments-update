ca126612f01b9053e95be9141c9691ba7c2eba79
aBdulAh220 <bitm-f19-041@superior.edu.pk>
2022-03-23 21:37:58 +0500

    login fragment Updated

--- app/src/main/java/com/example/loginwithfragements/MainActivity.java
+++ app/src/main/java/com/example/loginwithfragements/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.loginwithfragements;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
